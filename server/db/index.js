15a048e7694dc72e847f2767c6519b62e2aa131a
Andrew Lau <alau12004@yahoo.com>
2019-08-23 11:51:40 -0700

    complete mysql connection and schemas

--- server/db/index.js
+++ server/db/index.js
@@ -6,33 +6,36 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('CREATE DATABASE IF NOT EXISTS listings', function (error) {
-  if (error) {
-    throw error
+connection.query('CREATE DATABASE IF NOT EXISTS listings', function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack)
   } else {
     console.log('Listings DB created')
   };
 })
 
-connection.query('USE listings', function (error) {
-  if (error) throw error;
+connection.query('USE listings', function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack)
+  } else {
+    console.log('Database changed to listings')
+  };
 })
 
-connection.query('CREATE TABLE IF NOT EXISTS propertyListings ( id INT NOT NULL AUTO_INCREMENT, name VARCHAR(50) NOT NULL, property_description VARCHAR(50) PRIMARY KEY (id)', function (error) {
-    if (error) {
-      throw error
-    } else {
-      console.log('propertyListings Table created')
-    };
-  }
-})
+connection.query('CREATE TABLE IF NOT EXISTS propertyListings ( id INT NOT NULL AUTO_INCREMENT, name VARCHAR(50) NOT NULL, property_description VARCHAR(50), PRIMARY KEY (id)) ENGINE=INNODB;', function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack)
+  } else {
+    console.log('propertyListings Table created')
+  };
+});
 
-connection.query('CREATE TABLE IF NOT EXISTS photos ( id INT NOT NULL AUTO_INCREMENT, src VARCHAR(250) NOT NULL, FOREIGN KEY (propertyListings_id) REFERENCES properties(id) ON DELETE CASCADE', function (error) {
-  if (error) {
-    throw error
+connection.query('CREATE TABLE IF NOT EXISTS photos ( id INT NOT NULL AUTO_INCREMENT, src VARCHAR(250) NOT NULL, propertyListing_id INT, FOREIGN KEY (propertyListing_id) REFERENCES propertyListings(id) ON DELETE CASCADE, PRIMARY KEY(id))', function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack)
   } else {
     console.log('photos Table created')
-  }
-})
+  };
+});
 
 module.exports = connection
