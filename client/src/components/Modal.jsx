a49623554b226ffc49ce4bfcb67606909f081b2d
Andrew Lau <alau12004@yahoo.com>
2019-08-27 21:20:06 -0700

    WIP modal component

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -28,10 +28,15 @@ class Modal extends Component {
           <div styleName='main'><img src={this.props.photos[0]['src']}/></div>
         </div>
         <div styleName='gallery'>
+          <button>
+            <svg viewBox="0 0 24 24" styleName='svg'>
+            <path d="m23.25 24c-.19 0-.38-.07-.53-.22l-10.72-10.72-10.72 10.72c-.29.29-.77.29-1.06 0s-.29-.77 0-1.06l10.72-10.72-10.72-10.72c-.29-.29-.29-.77 0-1.06s.77-.29 1.06 0l10.72 10.72 10.72-10.72c.29-.29.77-.29 1.06 0s .29.77 0 1.06l-10.72 10.72 10.72 10.72c.29.29.29.77 0 1.06-.15.15-.34.22-.53.22" fill-rule="evenodd"></path>
+            </svg>
+          </button>
           <div styleName='carousel'>
             {this.props.photos.map((photo, index) => <div key={index} styleName='card'><img src={photo['src']}/></div>)}
           </div>
-          <div>
+          <div styleName='description'>
             Description
           </div>
         </div>
