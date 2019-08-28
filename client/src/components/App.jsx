01e5e1efbc0aff7818f7b936adaba98e43052c8c
Andrew Lau <alau12004@yahoo.com>
2019-08-27 20:32:50 -0700

    WIP modal component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import CSSModules from 'react-css-modules';
 import styles from './app.css';
 import axios from 'axios';
+import Modal from './Modal.jsx';
 
 class App extends Component {
   constructor(props) {
@@ -21,29 +22,29 @@ class App extends Component {
   render() {
     return (
       <div>
-      <Modal/>
-      <div styleName='main-container'>
-        <div styleName='photo-container'>
-          <div styleName="main-column"><img src={this.state.photos[0]['src']}/></div>
-          <div styleName="sub-column">
-            <div><img src={this.state.photos[1]['src']}/></div>
-            <div><img src={this.state.photos[2]['src']}/></div>
-          </div>
-          <div styleName="sub-column">
-            <div><img src={this.state.photos[3]['src']}/></div>
-            <div><img src={this.state.photos[4]['src']}/></div>
-          </div>
-          <div styleName="share-save">
-            <button onClick={this.onClick}>Share</button>
-            <button onClick={this.onClick}>Save</button>
-          </div>
-          <div styleName="view-photos">
-            <button>
-            View Photos
-            </button>
+      <Modal photos={this.state.photos}/>
+        <div styleName='main-container'>
+          <div styleName='photo-container'>
+            <div styleName="main-column"><img src={this.state.photos[0]['src']}/></div>
+            <div styleName="sub-column">
+              <div><img src={this.state.photos[1]['src']}/></div>
+              <div><img src={this.state.photos[2]['src']}/></div>
+            </div>
+            <div styleName="sub-column">
+              <div><img src={this.state.photos[3]['src']}/></div>
+              <div><img src={this.state.photos[4]['src']}/></div>
+            </div>
+            <div styleName="share-save">
+              <button onClick={this.onClick}>Share</button>
+              <button onClick={this.onClick}>Save</button>
+            </div>
+            <div styleName="view-photos">
+              <button>
+              View Photos
+              </button>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     )
   }
--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -2,18 +2,12 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import CSSModules from 'react-css-modules';
 import styles from './modal.css';
-import img1 from '../../data/1.jpg';
-import img2 from '../../data/2.jpg';
-import img3 from '../../data/3.jpg';
-import img4 from '../../data/4.jpg';
-import img5 from '../../data/5.jpg';
 
 class Modal extends Component {
   constructor(props) {
     super(props);
     this.state = {
       photoId: 1,
-      photos: [img1, img2, img3, img4, img5]
     }
     this.prevClick = this.prevClick.bind(this);
     this.nextClick = this.nextClick.bind(this);
@@ -31,11 +25,11 @@ class Modal extends Component {
     return (
       <div styleName='modal'>
         <div styleName='portrait'>
-        {/* <img src={img1}/> */}
+          <div styleName='main'><img src={this.props.photos[0]['src']}/></div>
         </div>
         <div styleName='gallery'>
-          <div>
-            {this.state.photos.map((photo, index) => <div key={index} styleName='card'><img src={photo}/></div>)}
+          <div styleName='carousel'>
+            {this.props.photos.map((photo, index) => <div key={index} styleName='card'><img src={photo['src']}/></div>)}
           </div>
           <div>
             Description
