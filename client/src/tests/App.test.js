eeb410d937d38a24f74259a8efad6faa41dcb22c
Andrew Lau <alau12004@yahoo.com>
2019-08-28 00:16:49 -0700

    WIP modal component

---
+++ client/src/tests/App.test.js
@@ -0,0 +1,5 @@
+import { shallow, mount, render } from 'enzyme';
+import App from '../components/App';
+import React from 'react';
+
+const wrapper = shallow(<App/>)
---
+++ client/src/tests/Modal.test.js
@@ -0,0 +1,5 @@
+import { shallow, mount, render } from 'enzyme';
+import Modal from '../components/Modal';
+import React from 'react';
+
+const wrapper = shallow(<Modal/>);
