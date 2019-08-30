4a5a910a93c95ad0ea7f50e855d7ca15ab4599e1
Andrew Lau <alau12004@yahoo.com>
2019-08-30 11:40:06 -0700

    WIP tests/modal components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -61,10 +61,24 @@ class App extends Component {
             <div styleName="share-save">
               <button onClick={this.onClick}>
                 <span>
-                <svg styleName="share-svg" viewBox="0 0 24 24" fill="currentColor" fill-opacity="0" stroke="currentColor" stroke-width="2.25" focusable="false" aria-hidden="true" role="presentation" stroke-linecap="round" stroke-linejoin="round"><g fill-rule="evenodd"><path d="m11.5 16v-15"></path><path d="m7.5 4 4-3 4 3"></path><path d="m5.4 9.5h-3.9v14h20v-14h-3.1"></path></g></svg>
+                  <div styleName="share-button-text">
+                    <div>
+                      <svg styleName="share-svg" viewBox="0 0 24 24" fill="currentColor" fill-opacity="0" stroke="currentColor" stroke-width="2.25" focusable="false" aria-hidden="true" role="presentation" stroke-linecap="round" stroke-linejoin="round"><g fillRule="evenodd"><path d="m11.5 16v-15"></path><path d="m7.5 4 4-3 4 3"></path><path d="m5.4 9.5h-3.9v14h20v-14h-3.1"></path></g></svg>
+                    </div>
+                    <div>Share</div>
+                  </div>
                 </span>
-                Share</button>
-              <button onClick={this.onClick}>Save</button>
+              </button>
+              <button onClick={this.onClick}>
+                <span>
+                  <div styleName="save-button-text">
+                    <div>
+                      <svg styleName="save-svg" viewBox="0 0 24 24" fill="currentColor" fill-opacity="0" stroke="#484848" stroke-width="2.25" focusable="false" aria-label="Save this listing." role="img" stroke-linecap="round" stroke-linejoin="round"><path d="m17.5 2.9c-2.1 0-4.1 1.3-5.4 2.8-1.6-1.6-3.8-3.2-6.2-2.7-1.5.2-2.9 1.2-3.6 2.6-2.3 4.1 1 8.3 3.9 11.1 1.4 1.3 2.8 2.5 4.3 3.6.4.3 1.1.9 1.6.9s1.2-.6 1.6-.9c3.2-2.3 6.6-5.1 8.2-8.8 1.5-3.4 0-8.6-4.4-8.6" stroke-linejoin="round"></path></svg>
+                    </div>
+                    <div>Save</div>
+                  </div>
+                </span>
+              </button>
             </div>
             <div styleName="view-photos">
               <button data-test="view-modal" onClick={this.onClick}>
