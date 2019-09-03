b29085108b0ce344df5cf99e3afd01fa6b9c6dda
Andrew Lau <alau12004@yahoo.com>
2019-09-02 17:48:38 -0700

    tests and modal components

--- client/index.js
+++ client/index.js
@@ -4,9 +4,6 @@ import App from './src/components/App.jsx';
 import axios from 'axios';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
-
-// TODO: Get property id from url item id instead of hardcode
-
 ReactDom.render(<Router><Route path="/listing/:id" component={App}/></Router>, document.getElementById('app'))
 
 
